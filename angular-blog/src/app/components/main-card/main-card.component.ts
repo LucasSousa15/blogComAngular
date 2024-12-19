c32cc2d9c3afc679115c038b96985b4ff322d523
Lucas Sousa <luck.sousa15@gmail.com>
2024-12-19 10:52:59 -0300

    detached components

--- angular-blog/src/app/components/main-card/main-card.component.ts
+++ angular-blog/src/app/components/main-card/main-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-main-card',
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './main-card.component.css'
 })
 export class MainCardComponent {
+
+  @Input()
+  photoCover: string = "";
+  @Input()
+  cardTitle:string = "";
+  @Input()
+  cardDescription:string = "";
+
+  constructor () {}
+
 
 }
